fe306471ddfffcb9b7859c10446b4fc7e77f7343
Ilya <bondarenkoilya2@gmail.com>
2025-01-13 05:19:53 +0300

    remove strict mode to avoid mess in console

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,12 +1,8 @@
-import { FC, StrictMode } from "react";
+import { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { Children } from "../types";
 
 export const Providers: FC<Children> = ({ children }) => {
-  return (
-    <StrictMode>
-      <BrowserRouter>{children}</BrowserRouter>
-    </StrictMode>
-  );
+  return <BrowserRouter>{children}</BrowserRouter>;
 };
