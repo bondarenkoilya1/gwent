578ae281893f5f0ea65ddcd4164ece0a629396a9
Ilya <bondarenkoilya2@gmail.com>
2025-01-13 05:20:36 +0300

    create basic version of card component

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,3 +1,27 @@
-export const Card = () => {
-  return <></>;
+import { FC } from "react";
+
+import { CardProps } from "../../../types/components";
+
+export const Card: FC<CardProps> = ({ name, description, type, points, ...attrs }) => {
+  return (
+    <li
+      style={{
+        border: "2px solid black",
+        padding: "10px",
+        width: "200px",
+        height: "250px",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+        position: "relative"
+      }}
+      {...attrs}>
+      <h2 style={{ marginBottom: "20px", fontWeight: "700", fontSize: "28px" }}>{name}</h2>
+      <p style={{ maxWidth: "120px" }}>{description}</p>
+      <p style={{ position: "absolute", top: "10px", right: "10px" }}>Type: {type}</p>
+      <p style={{ position: "absolute", bottom: "10px", left: "10px" }}>Points: {points}</p>
+    </li>
+  );
 };
