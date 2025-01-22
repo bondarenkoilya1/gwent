cc5bc8484ef67325bdab3320a30aed992c0ba8fc
Ilya <bondarenkoilya2@gmail.com>
2025-01-23 02:29:47 +0300

    create constants file, use exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,4 @@
 export * from "./Card";
+export * from "./CardRow";
+export * from "./PlayerBoard";
+export * from "./Deck";
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const CARDS_IN_DECK = 5;
+
+// temporary as json file, in the future - move to a real database
+export const NORTHERN_REALMS_CARDS_ARRAY = "./northernRealms.json";
+// todo: temporary const, rethink logic!!!
+export const NORTHERN_REALMS_CARDS_QUANTITY = 14;
