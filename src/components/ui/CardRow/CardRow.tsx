561c395ff368be9c9e7846fd80ca178024fc66f8
Ilya <bondarenkoilya2@gmail.com>
2025-01-24 05:09:38 +0300

    add styles for card-row component in separate file

--- src/components/ui/CardRow/CardRow.tsx
+++ src/components/ui/CardRow/CardRow.tsx
@@ -1,19 +1,9 @@
 import { FC } from "react";
 
+import { CardRowStyled } from "./styled";
+
 import { Children } from "../../../types";
 
 export const CardRow: FC<Children> = ({ children }) => {
-  return (
-    <ul
-      style={{
-        height: "251px",
-        border: "1px solid #000",
-        display: "flex",
-        width: "100%",
-        overflow: "hidden",
-        overflowX: "auto"
-      }}>
-      {children}
-    </ul>
-  );
+  return <CardRowStyled>{children}</CardRowStyled>;
 };
