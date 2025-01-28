d348b411732543bd156b56a465c26b081c4675ef
Ilya <bondarenkoilya2@gmail.com>
2025-01-29 02:24:09 +0300

    style rows on the board

--- src/components/ui/PlayerBoard/PlayerBoard.tsx
+++ src/components/ui/PlayerBoard/PlayerBoard.tsx
@@ -1,21 +1,31 @@
 import { FC } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-import { PlayerBoardStyled } from "./styled";
+import { CardRowStyles, PlayerBoardStyled } from "./styled";
+import styled from "@emotion/styled";
 
 import { CARD_TYPES } from "../../../constants";
 import { CardsOnBoardArray, CardType } from "../../../types";
 import { Card } from "../Card";
 import { CardRow } from "../CardRow";
 
+const CardRowStyled = styled(CardRow)(CardRowStyles);
+
 export const PlayerBoard: FC<CardsOnBoardArray> = ({ cardsOnBoard }) => {
   const renderRowsByCardTypes = () =>
     CARD_TYPES.map((type: CardType, index) => (
-      <CardRow type={type} key={uuidv4()}>
-        {cardsOnBoard[index].cards.map(({ name, description, type, points }) => (
-          <Card key={uuidv4()} name={name} description={description} type={type} points={points} />
+      <CardRowStyled type={type} key={uuidv4()}>
+        {cardsOnBoard[index].cards.map(({ id, name, description, type, points }) => (
+          <Card
+            key={id}
+            id={id}
+            name={name}
+            description={description}
+            type={type}
+            points={points}
+          />
         ))}
-      </CardRow>
+      </CardRowStyled>
     ));
 
   return <PlayerBoardStyled>{renderRowsByCardTypes()}</PlayerBoardStyled>;
