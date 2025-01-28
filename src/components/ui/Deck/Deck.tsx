60412fb49d42c7e69aa4955bc95fde28162ef503
Ilya <bondarenkoilya2@gmail.com>
2025-01-29 02:23:56 +0300

    add logic to remove card from deck when it sent to the board

--- src/components/ui/Deck/Deck.tsx
+++ src/components/ui/Deck/Deck.tsx
@@ -1,63 +1,68 @@
 import { FC } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-import { DeckStyled, LoadingMessageStyled, TemporaryButtonStyled, TitleStyled } from "./styled";
+import { DeckStyled, LoadingMessageStyled, TitleStyled } from "./styled";
 
-import {
-  CARD_TYPES,
-  NORTHERN_REALMS_CARDS_ARRAY,
-  NORTHERN_REALMS_CARDS_QUANTITY
-} from "../../../constants";
+import { NORTHERN_REALMS_CARDS_ARRAY, NORTHERN_REALMS_CARDS_QUANTITY } from "../../../constants";
 import { useCardSetup } from "../../../hooks";
-import { CardsOnBoardUpdater, CardType } from "../../../types";
+import { CardProps, CardsOnBoardUpdater } from "../../../types";
 import { Card } from "../Card";
 import { CardRow } from "../CardRow";
 import { Error } from "../Error";
 
 export const Deck: FC<CardsOnBoardUpdater> = ({ setCardsOnBoard }) => {
-  const { cardsInDeck, loading, error } = useCardSetup(
+  const { cardsInDeck, setCardsInDeck, loading, error } = useCardSetup(
     NORTHERN_REALMS_CARDS_ARRAY,
     NORTHERN_REALMS_CARDS_QUANTITY
   );
 
   if (loading) return <LoadingMessageStyled>Loading...</LoadingMessageStyled>;
 
-  const addCardToBoard = (selectedRowType: CardType) =>
+  const removeCardFromDeck = (cardId: string) => {
+    setCardsInDeck((prevDeck: CardProps[]) =>
+      prevDeck.filter((deckCard) => deckCard.id !== cardId)
+    );
+  };
+
+  const addCardToBoard = (card: CardProps) => {
+    const selectedRowType = card.type;
+
     setCardsOnBoard((prevState) =>
       prevState.map((row) =>
         row.type === selectedRowType
           ? {
               ...row,
-              cards: [
-                ...row.cards,
-                {
-                  name: `${Math.floor(Math.random() * 100)}`,
-                  description: "card!",
-                  type: selectedRowType,
-                  points: Math.floor(Math.random() * 10) + 1
-                }
-              ]
+              cards: [...row.cards, card]
             }
           : row
       )
     );
 
+    removeCardFromDeck(card.id);
+  };
+
   return (
     <DeckStyled>
-      {CARD_TYPES.map((type) => (
-        <TemporaryButtonStyled key={uuidv4()} onClick={() => addCardToBoard(type)}>
-          Add {type} fight card to the board
-        </TemporaryButtonStyled>
-      ))}
-
       {/*TODO: Firstly i create branch for creating an error component; Then branch for publishing changes about card transferring*/}
       {error && <Error unspecifiedErrorMessage={error} />}
+
       <TitleStyled>Your Deck</TitleStyled>
       <CardRow type="deck">
-        {cardsInDeck.map(({ name, description, type, points }) => (
-          // TODO: OnClick should transfer card to the board and remove from the deck
-          <Card name={name} description={description} type={type} points={points} key={uuidv4()} />
-        ))}
+        {cardsInDeck.map((card: CardProps) => {
+          const { id, name, description, type, points } = card;
+
+          return (
+            <Card
+              id={id}
+              name={name}
+              description={description}
+              type={type}
+              points={points}
+              key={uuidv4()}
+              onClick={() => addCardToBoard(card)}
+            />
+          );
+        })}
       </CardRow>
     </DeckStyled>
   );
