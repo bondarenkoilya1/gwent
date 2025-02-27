ba2d6b0bf55ff018c32095eaca7a2302d0b5df59
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-02-27 09:56:28 +0300

    update image reset styles

--- src/components/ui/Image/Image.tsx
+++ src/components/ui/Image/Image.tsx
@@ -4,10 +4,6 @@ import { ImageStyled } from "./styled";
 
 import { ImageProps } from "../../../types";
 
-export const Image: FC<ImageProps> = ({ src, alt }) => {
-  if (!src) return <ImageStyled src="https://placehold.co/100x100" alt={alt} />;
-
-  return <ImageStyled src={src} alt={alt} />;
+export const Image: FC<ImageProps> = ({ src = "https://placehold.co/100x100", alt, loading }) => {
+  return <ImageStyled src={src} alt={alt} loading={loading} />;
 };
-
-// lazy loading prop
