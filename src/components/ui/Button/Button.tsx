ea545948e9c92677d44aded45357f0ab2e1c2945
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-02-27 09:33:16 +0300

    create template for button variants

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,17 +4,27 @@ import { ButtonStyled } from "./styled";
 
 import { ButtonProps } from "../../../types";
 
+// TODO: Create theme for colors with emotion
+
 export const Button: FC<ButtonProps> = ({
   className,
   children,
   type = "button",
-  variant = "primary"
+  variant = "primary",
+  hasIcon
 }) => {
-  return (
-    <ButtonStyled className={className} type={type} variant={variant}>
-      {children}
-    </ButtonStyled>
-  );
+  switch (hasIcon) {
+    case "onLeft":
+      return 1;
+    case "onRight":
+      return 2;
+    default:
+      return (
+        <ButtonStyled className={className} type={type} variant={variant}>
+          {children}
+        </ButtonStyled>
+      );
+  }
 };
 
 // todo: on lint i need to find all .styled.*ts* imports and remove ,ts
